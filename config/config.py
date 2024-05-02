0b9f52606cbfe3520c003d6ecf7f5f8b1cec43cf
natha <108113193+Mandaiya@users.noreply.github.com>
2024-05-02 21:11:32 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -354,6 +354,3 @@ if TELEGRAM_VIDEO_URL:
                 "[ERROR] - Your TELEGRAM_VIDEO_URL url is wrong. Please ensure that it starts with https://"
             )
             sys.exit()
-
-
-    sys.exit()
