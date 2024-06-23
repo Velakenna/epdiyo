22ef7fdf894b3fa9c926fcae28bbcbd52c4fa8e1
Velakenna <141400352+Velakenna@users.noreply.github.com>
2024-06-23 11:10:59 +0530

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -60,7 +60,8 @@ def start_pannel(_):
     return buttons
 
 
-def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+#def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+def private_panel(_, BOT_USERNAME):
     buttons = [
         [                  
             InlineKeyboardButton(
